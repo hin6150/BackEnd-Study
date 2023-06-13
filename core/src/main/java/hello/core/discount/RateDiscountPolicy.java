3a0e931797c8830cf06ad85078f2f1ed1a47a6d1
hin6150 <hin6150@gmail.com>
2023-06-13 14:31:14 +0900

    의존관계 자동 주입

--- core/src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ core/src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -1,13 +1,16 @@
 package hello.core.discount;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import org.springframework.stereotype.Component;
 
 @Component
-public class RateDiscountPolicy implements DiscountPolicy{
+@MainDiscountPolicy
+public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountPercent = 10;
+
     @Override
     public int discount(Member member, int price) {
         if (member.getGrade() == Grade.VIP) {
